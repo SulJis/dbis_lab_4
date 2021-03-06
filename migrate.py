191f7c17c6272448f6f45c6689c91a5c678e798d
suljis <mx.slipchenko@gmail.com>
2021-03-06 13:36:38 +0200

    Initiated connection

--- migrate.py
+++ migrate.py
@@ -1,1 +1,9 @@
 import psycopg2
+
+conn = psycopg2.connect(dbname="postgres", user="postgres", password="vfrcbv2001", host="localhost", port="5000")
+cursor = conn.cursor()
+
+conn.commit()
+
+cursor.close()
+conn.close()
